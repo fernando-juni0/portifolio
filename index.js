803f5d6f2f24b672c7ae8502e545ddc69b972e63
Fernando <junio132sj@gmail.com>
2022-12-23 18:10:03 -0300

    fix

---
+++ index.js
@@ -0,0 +1,60 @@
+//-------------importes------------
+const express = require('express')
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const session = require('express-session')
+const path = require('path');
+
+
+//------------Configs--------------
+const app = express();
+
+require('dotenv').config()
+
+
+app.use(session({
+    secret: process.env.SECRET, 
+    resave: false, 
+    saveUninitialized: true,
+}))
+
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
+app.use(express.static('views'));
+app.use(express.static('public'));
+app.use(express.static('uploads'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs');
+
+
+
+
+//-----------------GET--------------------
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+
+//----------------POST--------------------
+
+
+
+
+
+
+
+
+
+
+
+//----------------SERVER-------------------
+
+const port = process.env.PORT || 80
+app.listen(port,()=>{
+    console.log(`Servidor rodando na porta ${port}` );
+});
